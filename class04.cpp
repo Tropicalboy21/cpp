c48bf8b5aa0b895fe39ecfee7501939fffcafa95
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-06-03 19:52:45 -0600

    practice

--- class04.cpp
+++ class04.cpp
@@ -1,9 +1,19 @@
-#include <iostream>;
-#include <string>;
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     int cant = 0;
+    cout << "Ingrese la cantidad de edades: ";
+    cin >> cant;
+
+    int edades[cant];
+
+    for (int i = 0; i < cant; i++)
+    {
+        cout << "Edad posicion No " << i + 1 << ": ";
+        cin >> edades[i];
+    }
 }
