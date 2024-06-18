dd95e018e07c832155ca5cc76a9ca878c4729890
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-06-17 19:11:48 -0600

    class 06

--- data_structures/punteros.cpp
+++ data_structures/punteros.cpp
@@ -6,13 +6,13 @@ void pause()
     std::cout << "\nPresione Enter para continuar...";
     cin.ignore();
     cin.get();
-    system("cls");
+    system("clear");
 }
 
 int main()
 {
 
-    int a;
+    int a, b, c;
     int *aPtr;
 
     a = 7;
@@ -22,6 +22,22 @@ int main()
     cout << "\nLa direccion de la variable a es : " << &a << endl;
 
     cout << "\nEl valor de aPtr es : " << aPtr << endl;
+
+    b = *aPtr * 3;
+
+    cout << "\nEl valor de la variable b es : " << b << endl;
+
+    aPtr = NULL;
+
+    cout << "\nLa direccion de la variable a es: " << &a << endl;
+
+    cout << "\nEl valor de aPtr es : " << aPtr << endl;
+
+    aPtr = &b;
+
+    c = *aPtr + 1500;
+
+    cout << "\nEl valor de la variable c es : " << c << endl;
 
     pause();
 }
---
+++ data_structures/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    // aPtr = NULL;
