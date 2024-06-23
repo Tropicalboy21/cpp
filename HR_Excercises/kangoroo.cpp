5184ae2c63781381fc4ae0bf58d0526837f2a97d
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-06-23 05:47:08 -0600

    Practice

--- HR_Excercises/kangoroo.cpp
+++ HR_Excercises/kangoroo.cpp
@@ -5,11 +5,19 @@ string kangaroo(int x1, int v1, int x2, int v2)
 {
 
     if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0)
+    {
+        cout << "YES" << endl;
         return "YES";
-    return "NO";
+    }
+    else
+    {
+        cout << "NO" << endl;
+        return "NO";
+    }
 }
 
 int main()
 {
     kangaroo(0, 2, 5, 3);
+    kangaroo(0, 3, 4, 2);
 }
